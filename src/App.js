fa279ed8d7bf17de20409e2be2d7836690236870
ChandGoyal <chandgoyal017@gmail.com>
2024-02-01 18:09:40 +0530

    Create Table App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [data, setData] = useState([
+    { date: "2022-09-01", views: 100, article: "Article 1" },
+    { date: "2023-09-01", views: 100, article: "Article 1" },
+    { date: "2023-09-02", views: 150, article: "Article 2" },
+    { date: "2023-09-02", views: 120, article: "Article 3" },
+    { date: "2020-09-03", views: 200, article: "Article 4" },
+  ]);
+  const [sortByDateAsc, setSortByDateAsc] = useState(true);
+  const [sortByViewsAsc, setSortByViewsAsc] = useState(true);
+
+  const sortByDate = () => {
+    const sortedData = [...data].sort((a, b) => {
+      if (a.date === b.date) {
+        return sortByViewsAsc ? b.views - a.views : a.views - b.views;
+      }
+      return sortByDateAsc
+        ? new Date(b.date) - new Date(a.date)
+        : new Date(a.date) - new Date(b.date);
+    });
+
+    setData(sortedData);
+    setSortByDateAsc(!sortByDateAsc);
+  };
+
+  const sortByViews = () => {
+    const sortedData = [...data].sort((a, b) => {
+      if (a.views === b.views) {
+        return sortByDateAsc
+          ? new Date(b.date) - new Date(a.date)
+          : new Date(a.date) - new Date(b.date);
+      }
+      return sortByViewsAsc ? b.views - a.views : a.views - b.views;
+    });
+
+    setData(sortedData);
+    setSortByViewsAsc(!sortByViewsAsc);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Date and Views Table</h1>
+      <button onClick={sortByDate}>Sort by Date</button>
+      <button onClick={sortByViews}>Sort by Views</button>
+      <table>
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Views</th>
+            <th>Article</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item, index) => (
+            <tr key={index}>
+              <td>{item.date}</td>
+              <td>{item.views}</td>
+              <td>{item.article}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
